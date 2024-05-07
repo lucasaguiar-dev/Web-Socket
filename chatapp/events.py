a7bc4474ae04c8ab65b0167f94d74bf0a6087c84
Lucas Aguiar <lucas.9526@hotmail.com>
2024-05-06 21:50:09 -0300

    Create log for user join

--- chatapp/events.py
+++ chatapp/events.py
@@ -3,3 +3,7 @@ from .extensions import socketio
 @socketio.on("connect")
 def handle_connect():
     print("Client connected!")
+
+@socketio.on("user_join")
+def handle_user_join(username):
+    print(f"User {username} joined!")
